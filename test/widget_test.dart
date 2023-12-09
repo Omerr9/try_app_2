6b43e022b4502ce546ac1bf583ccb095b1d4e3f9
huzaechxx <huzaifazahid87@gmail.com>
2023-12-10 03:51:03 +0500

    ..
    ..

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:try_app_2/main.dart';
+import 'package:pracflut1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
